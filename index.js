2f48dca2d5a483afc3b3a842e3d85dd1b17187af
Austin Song <austinhssong@gmail.com>
2023-02-07 02:19:32 -0500

    initial commit

--- index.js
+++ index.js
@@ -1,36 +1,43 @@
-import React from 'react'
-import { createRoot } from 'react-dom/client'
+import React from "react";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 
 const Red = (props) => {
   return (
-    <div className='red'>
+    <div className="red">
       <h1>RED</h1>
     </div>
-  )
-}
+  );
+};
 
 const Blue = (props) => {
   return (
-    <div className='blue'>
+    <div className="blue">
       <h1>BLUE</h1>
     </div>
-  )
-}
+  );
+};
 
 const Main = (props) => {
   return (
-    <div id='container'>
+    <BrowserRouter>
+      <div id="container">
+        <div id="navbar">
+          <Link to = "/red">Go To Red</Link>
+          <Link to = "/blue">Go To Blue</Link>
+        </div>
 
-      <div id='navbar'>
-        {/* navigation here */}
+        <div id="main-section">
+            <Routes>
+              <Route path="/blue" element={<Blue />} />
+              <Route path="/red" element={<Red />} />
+              <Route exact path = "/" element = {<Red />}/>
+            </Routes>
+        </div>
       </div>
+    </BrowserRouter>
+  );
+};
 
-      <div id='main-section'>
-        {/* routes here */}
-      </div>
-    </div>
-  )
-}
-
-const root = createRoot(document.getElementById('app'))
-root.render(<Main />)
+const root = createRoot(document.getElementById("app"));
+root.render(<Main />);
